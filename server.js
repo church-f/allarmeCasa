7d44f04f271039980b5ef38bff9d71357a086254
church-f <68963960+church-f@users.noreply.github.com>
2024-09-01 17:31:40 +0200

    aggiunto firebase

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded());
 const bot = new Telegraf('7234218457:AAFeRmBk_T3JI5z7BD9yjww-Lud8UN-TMUY')
 
+//firebase 
+var firebase = require('firebase/compat/app');
+require('firebase/compat/database');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCq-gD05dihbZgntvnhX1RXxaAEEUxsFT0",
+    authDomain: "allarmedicasa-dc621.firebaseapp.com",
+    projectId: "allarmedicasa-dc621",
+    storageBucket: "allarmedicasa-dc621.appspot.com",
+    messagingSenderId: "881462219879",
+    appId: "1:881462219879:web:ebae1cddc700903082b0c2",
+    databaseURL: "https://allarmedicasa-dc621-default-rtdb.europe-west1.firebasedatabase.app"
+  };
+firebase.initializeApp(firebaseConfig)
+
+var data 
+firebase.database().ref().on('value', (snapshot) => {
+    data = snapshot.val();
+    console.log(`dati caricati da firebase`)
+});
+
+
+function nnu(x) {
+    return x !== undefined || x !== null;
+  }
 server.listen(PORT)
 let socketAddressEsp = null
 io.on('connection', socket=>{
@@ -22,41 +47,50 @@ io.on('connection', socket=>{
 
 })
 
-let buzzerIsOn = false
-let sendMessage = false
-
 app.post('/newMovement', (req, res)=>{
     console.log('arrivata')
     console.log(req.body)
-    if(sendMessage){
+    if(nnu(data['sendMessage']) && data['sendMessage']){
         bot.telegram.sendMessage(862519383, 'sensore di movimento scattato in garage', {
         })
     }
-    res.send({buzzerIsOn: buzzerIsOn})
+    res.send({buzzerIsOn: data['buzzerIsOn']})
 })
 
 //comandi per il bot
 
 //comandi per attivate e disattivare il suono del buzzer allo scattare di un sensore
-bot.command('attivaBuzzer', (ctx) => {
-    buzzerIsOn = true
+bot.command('attiva_buzzer', (ctx) => {
+    firebase
+        .database()
+        .ref('buzzerIsOn')
+        .set(true);
     ctx.reply('Buzzer attivato');
     console.log('buzzer attivato')
 });
-bot.command('disattivaBuzzer', (ctx) => {
-    buzzerIsOn = false
+bot.command('disattiva_buzzer', (ctx) => {
+    firebase
+        .database()
+        .ref('buzzerIsOn')
+        .set(false);
     ctx.reply('Buzzer disattivato');
     console.log('buzzer disattivato')
 });
 
 //comandi per attivare e disattivare l'invio del messaggio allo scattare del sensore
-bot.command('attivaMessaggio', (ctx) => {
-    sendMessage = true
+bot.command('attiva_messaggi', (ctx) => {
+    firebase
+        .database()
+        .ref('sendMessage')
+        .set(true);
     ctx.reply('Messaggi attivati');
     console.log('Messaggi attivati')
 });
-bot.command('disattivaMessaggio', (ctx) => {
-    sendMessage = false
+bot.command('disattiva_messaggi', (ctx) => {
+    firebase
+        .database()
+        .ref('sendMessage')
+        .set(false);
     ctx.reply('Messaggi disattivati');
     console.log('Messaggi disattivati')
 });
